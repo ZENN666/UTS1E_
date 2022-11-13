c8b19746760dfe7f1ed40ba63ae9923ac8b888df
ZENN666 <zaenulmilahi007@gmail.com>
2022-11-14 04:40:18 +0700

    y

---
+++ src/nomer1.java
@@ -0,0 +1,8 @@
+public class nomer1 {
+    public static void main(String[] args) {
+        double a = 25.8;
+        float b = 17;
+        double hasil = a*b;
+        System.out.println(hasil);
+    }
+}
