b845d4b45e59389afd39797690afbb30740dc6cc
ZENN666 <zaenulmilahi007@gmail.com>
2022-11-14 12:05:25 +0700

    y

--- src/JAWABAN03.java
+++ src/JAWABAN03.java
@@ -1,12 +1,51 @@
 import java.util.Scanner;
 
 public class JAWABAN03 {
-    public static void main(String[] args) {
-        System.out.println("Program data penumpang bus");
-        System.out.println("***************** KFC *****************");
+    public class tes {
+        static int JumlahTopLevel = 0;
+        static int JumlahMiddleLevel = 0;
+        static int JumlahRegularStaff = 0;
+        static int JumlahStaff = 0;
 
-        Scanner PenumpangBis = new Scanner(System.in);
-        System.out.println("masukan nama = ");
-        String nama =PenumpangBis.nextLine();
+        static boolean success = false;
+
+        public static void main(String[] args) {
+            String[] businfo = {"01-TopLevel", "02-MiddleLevel", "03-RegularStaff", "04-Staff"};
+
+            for (int i=0; i <200; i++){
+                NIK();
+            }
+        }
+
+        static void CekNIKPegawaiKFC(String NIK){
+            //cek apakah nik kurang dari 10 figit
+            if (NIK.length() <= 10){
+                //Dinyatakan benar jika kurang dari 10 digit
+                //Cek apakah NIK mengandung huruf c01
+                //Jika iya maka bis Top level bertambah 1 penumpang
+                if (NIK.contains("C01")){
+                    success=true;
+                    JumlahTopLevel++;
+                } else if (NIK.contains("J02")){
+                    success=true;
+                    JumlahMiddleLevel++;
+                } else if (NIK.contains("N03")) {
+                    success=true;
+                    JumlahRegularStaff++;
+
+                } else if (NIK.contains("P04")) {
+                    success=true;
+                    JumlahStaff++;
+
+                } else {
+                    success=false;
+                    System.out.println("Maaf NIK anda tidak terdaftar, silahkan coba lagi");
+                }
+            }else {
+                success=false;
+                System.out.println("NIK tidak valid");
+
+            }
+            }
+        }
     }
-}
